5ac844542b8c33187548d70073da790310c67eff
guychenya <guy.chenya@gmail.com>
2025-11-24 22:06:07 +0200

    Make AI sidebar integrated into layout (Canvas mode)

--- App.tsx
+++ App.tsx
@@ -875,7 +875,7 @@ const EditorWorkspace = () => {
       )}
 
       {/* Main Content */}
-      <div className="flex-1 flex flex-col min-w-0 relative h-screen">
+      <div className="flex-1 flex flex-col min-w-0 relative h-screen" style={showAIChat ? { marginRight: `${aiPanelWidth}px` } : {}}>
         
         {/* Header */}
         {!focusMode && (
