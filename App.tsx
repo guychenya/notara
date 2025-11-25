0d7368e26565743cb11145dd95fa1dd7fabbfde8
guychenya <guy.chenya@gmail.com>
2025-11-25 10:57:16 +0200

    Fix AI sidebar overlapping content - add overflow-x-hidden

--- App.tsx
+++ App.tsx
@@ -999,7 +999,7 @@ const EditorWorkspace = () => {
       )}
 
       {/* Main Content */}
-      <div className="flex-1 flex flex-col min-w-0 relative h-screen" style={showAIChat ? { marginRight: `${aiPanelWidth}px` } : {}}>
+      <div className="flex-1 flex flex-col min-w-0 relative h-screen overflow-x-hidden" style={showAIChat ? { marginRight: `${aiPanelWidth}px` } : {}}>
         
         {/* Header */}
         {!focusMode && (
